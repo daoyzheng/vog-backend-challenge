e6d53cc1b906a9c2601db8a3c0eca2d85f10c872
Dao Zheng <dao.zheng@vogcalgaryappdeveloper.com>
2021-03-10 00:56:49 -0700

    Add TESTModule method

--- VogCodeChallenge.console/Program.cs
+++ VogCodeChallenge.console/Program.cs
@@ -8,6 +8,15 @@ namespace VogCodeChallenge.console
         static void Main(string[] args)
         {
             QuestionClass.NamesList.ForEach(name => Console.WriteLine(name));
+            try
+            {
+                var result = QuestionClass.TESTModule(0);
+                Console.WriteLine(result);
+            } catch(ArgumentOutOfRangeException outOfRange)
+            {
+                Console.WriteLine(outOfRange);
+            }
+ 
         }
     }
 
@@ -19,6 +28,28 @@ namespace VogCodeChallenge.console
             "Jeffrey",
             "John"
         };
+
+        public static object TESTModule(object input)
+        {
+            switch(input)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    return (int)input * 2;
+                case int value when value > 4:
+                    return value * 3;
+                case int value when value < 1:
+                    throw new ArgumentOutOfRangeException();
+                case float value when ( value == 1.0f || value == 2.0f ):
+                    return 3.0f;
+                case string value:
+                    return value.ToUpper();
+                default:
+                    return input;
+            }
+        }
     }
 
 }
