0e4767dad4d7e8262b955f2e583bd05b400ca7c3
Dao Zheng <dao.zheng@vogcalgaryappdeveloper.com>
2021-03-10 00:30:59 -0700

    Add console project and iterate through names list

---
+++ VogCodeChallenge.console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VogCodeChallenge.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.NamesList.ForEach(name => Console.WriteLine(name));
+        }
+    }
+
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John"
+        };
+    }
+
+}
